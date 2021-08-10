57267972f3223a8cfb3d3f97e38e2cbadccf6040
BULUTS <BULUTS@BULUTS>
2021-08-10 14:54:55 +0300

    istatistik ödevi

--- MvcProjeKampi/Controllers/İstatistikController.cs
+++ MvcProjeKampi/Controllers/İstatistikController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrate;
+using DataAccessLayer.EntityFramework;
+using MvcProjeKampi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,7 @@ namespace MvcProjeKampi.Controllers
         // GET: İstatistik
         public ActionResult Index()
         {
-            return View();
+            return View( new State().GetModelStyle());
         }
     }
 }
---
+++ MvcProjeKampi/Models/State.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Concrate;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrate;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjeKampi.Models
+{
+    public class State
+    {
+        //CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        Context c = new Context();
+        public StateModelStyle GetModelStyle()
+        {
+            StateModelStyle models = new StateModelStyle();
+            models.toplamkategori = c.Categories.ToList().Count();
+            models.basliksayisi = c.Headings.Where(x => x.CategoryID == 17).ToList().Count;
+            models.yazarsayisi = c.Writers.Where(x => x.WriterName.Contains("a")).ToList().Count;
+            //models.enfbaslik = c.Headings.Where(x=>x.CategoryID) == x.CategoryID ).ToList().Count();
+            models.durum = c.Categories.Where(i => i.CategoryStatus == true).ToList().Count();
+            models.durum2 = c.Categories.Where(i => i.CategoryStatus == false).ToList().Count();
+            models.durum3 = models.durum - models.durum2; 
+
+            return models;
+        }
+    }
+    public class StateModelStyle
+    {
+        public int toplamkategori { get; set; }
+        public int basliksayisi { get; set; }
+        public int yazarsayisi { get; set; }
+        public int durum { get; set; }
+        public int durum2 { get; set; }
+        public int durum3 { get; set; }
+        
+
+    }
+}
