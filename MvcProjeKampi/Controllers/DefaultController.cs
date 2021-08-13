b03f939760ac23bd8d1911a578db0cbc4f5b1434
BULUTS <BULUTS@BULUTS>
2021-08-13 12:02:54 +0300

    Mvc Proje Kampı 81 Vitrin Sayfası

---
+++ MvcProjeKampi/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrate;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        public ActionResult Headings()
+        {
+            var headinglist = hm.GetList();
+            return View(headinglist);
+        }
+
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
